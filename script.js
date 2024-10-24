1b2ac96068c631f9f8c7ea04a4c030b6d9588f55
Azhaghiri <ganadhan6528@gmail.com>
2024-10-24 19:22:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-//your JS code here. If required.
-document.querySelector('input[type="button"]').addEventListener('click', function() {
-    const selectElement = document.getElementById('colorSelect');
-    const selectedIndex = selectElement.selectedIndex; // Get the index of the selected option
-    
-    if (selectedIndex !== -1) { // Ensure an option is selected
-        selectElement.remove(selectedIndex); // Remove the selected option
-    }
-});
+function removeSelectedOption() {
+				const selectElement = document.getElementById('colorSelect');
+				const selectedIndex = selectElement.selectedIndex; // Get the index of the selected option
+				
+				if (selectedIndex !== -1) { // Ensure an option is selected
+					selectElement.remove(selectedIndex); // Remove the selected option
+				}
+			}
+
+			// Attach the event listener to the button, using the named function
+			const button = document.querySelector('input[type="button"]');
+			button.addEventListener('click', removeSelectedOption);
